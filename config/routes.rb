f1ca793733a6ebf09ee3aeee7a7aa2caa70799e0
Michael Wnek <beatawnek@Beatas-MacBook-Pro.local>
2019-04-25 17:07:55 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
+  resources :posts, only: [:index, :show]
+  get '/students/:id/activate', to 'student#activate', as: 'activate_student'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
